3dc5f2b71c687c6bda220d334075e9a6fe481410
Emidio Valeretto <emidio.valereto@gmail.com>
2025-10-23 12:33:31 +0100

    feat(party): add URL routes for party detail pages

--- party/urls.py
+++ party/urls.py
@@ -5,4 +5,12 @@ list_parties_urlpatterns = [
     path("", views.PartyListView.as_view(), name="page_party_list"),
 ]
 
-urlpatterns = list_parties_urlpatterns
+party_detail_urlpatterns = [
+    path("party/<uuid:party_uuid>/", views.PartyDetailView.as_view(), name="page_party_detail"),
+    path("party/<uuid:party_uuid>/details/",
+         views.PartyDetailPartial.as_view(),
+         name="partial_party_detail"),
+
+]
+
+urlpatterns = list_parties_urlpatterns + party_detail_urlpatterns
